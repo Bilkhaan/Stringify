856afd1bdc986e6996d52cfb3887afa18dce34ac
Bilal Khan <bilal.khan@square63.com>
2015-08-15 19:12:38 +0500

    Added the functionality to combine arrays along with strings

--- lib/stringify.rb
+++ lib/stringify.rb
@@ -2,6 +2,7 @@ require "stringify/version"
 
 module Stringify
   def self.combine_with(connector, *args)
+    args.flatten!
     return args.first if args.count == 1
     strings = args.map { |string| string.to_s }
     connector = connector.to_s
